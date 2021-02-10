83207c93e4e39e92d50dbebdd831e3f9d9eb5025
kenkenm43 <nattawat.2543@mail.kmutt.ac.th>
2021-02-10 16:03:19 +0700

    first commit

---
+++ 62130500125_Groupwork_3/main.js
@@ -0,0 +1,21 @@
+const app = {
+    data() {
+        return {
+            objs:[{img:'https://lifestyle.campus-star.com/app/uploads/2017/11/hanamichi-sakuragi-anime-slam-dunk-wallpaper.jpg',title:'Hanamichi Sakuragi',done:false},
+        {img:'http://topicstock.pantip.com/chalermthai/topicstock/2008/07/A6779924/A6779924-22.jpg',title:'Kaede Rukawa',done:false},
+        {img:'https://pbs.twimg.com/profile_images/1856896081/Sendoh_A_P_400x400.jpg',title:"Akira Sendoh",done:false}]
+        }
+    },
+    methods: {
+        toggleDone(index){
+            this.objs[index].done = !this.objs[index].done
+        }
+    },
+    computed: {
+        countUndone(){
+            return this.objs.filter( o => o.done ).length
+        }
+    }
+
+}
+var mountedApp = Vue.createApp(app).mount('#app')
